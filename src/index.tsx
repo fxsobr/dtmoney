0672223701bbdaa664bf4ca529eb9d2e6b395d8f
Marcelo Ceolin <mceolin@unidavi.edu.br>
2022-03-14 17:16:53 -0300

    listando transações

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,37 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Pudim',
+          type: 'withdraw',
+          category: 'Padaria',
+          amount: 15,
+          createdAt: new Date('2022-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Sorvete',
+          type: 'withdraw',
+          category: 'Supermercado',
+          amount: 28,
+          createdAt: new Date('2022-02-13 09:00:00')
+        },
+        {
+          id: 3,
+          title: 'Freelance',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 3000,
+          createdAt: new Date('2022-02-15 09:00:00')
+        }
+      ],
+    })
+  },
+
   routes() {
     this.namespace = '/api';
 
